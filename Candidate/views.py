926a8c1a4a46e3db9fcf152dc2f56cb88b0e786b
Nilesh Parab <nileshparab5623@gmail.com>
2022-01-10 20:18:18 +0530

    Setting up project

---
+++ Candidate/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def job(request):
+    return render(request,'prejob.html')
+
+def index(request):
+    return render(request,'index.html')
+
